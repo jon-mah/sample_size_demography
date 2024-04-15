bc46ea1da754da0604d1a89697f66bb322d7547d
jon-mah <jonmah@uw.edu>
2024-04-15 14:55:42 -0700

    merge

--- Scripts/downsample_sfs.bash
+++ Scripts/downsample_sfs.bash
@@ -18,15 +18,15 @@ sample_size=$SGE_TASK_ID
 # python downsample_sfs.py ../Data/p_copri_core_empirical_nonsyn_sfs.txt ${sample_size} ../Analysis/p_copri_core_${sample_size}/nonsyn
 
 # 1000Genomes EUR data
-python downsample_sfs.py ../Data/kim_et_al_2017_1kg_syn.txt ${sample_size} ../Analysis/1kg_EUR_${sample_size}/syn
-# python downsample_sfs.py ../Data/kim_et_al_2017_1kg_nonsyn.txt ${sample_size} ../Analysis/1kg_EUR_${sample_size}/nonsyn
+# python downsample_sfs.py ../Data/kim_et_al_2017_1kg_syn.txt ${sample_size} ../Analysis/1kg_EUR_${sample_size}/syn
+python downsample_sfs.py ../Data/kim_et_al_2017_1kg_nonsyn.txt ${sample_size} ../Analysis/1kg_EUR_${sample_size}/nonsyn
 
 # Simulated Tennessen data
-python downsample_sfs.py ../Simulations/ooa_864_sfs/dadi/pop1.sfs ${sample_size} ../Analysis/ooa_simulated_${sample_size}/syn
+# python downsample_sfs.py ../Simulations/ooa_864_sfs/dadi/pop1.sfs ${sample_size} ../Analysis/ooa_simulated_${sample_size}/syn
 
 # Simple simulations
-python downsample_sfs.py ../Simulations/simple_simulations/TwoEpochContraction_concat.sfs ${sample_size} ../Analysis/TwoEpochContraction_${sample_size}/syn
-python downsample_sfs.py ../Simulations/simple_simulations/ThreeEpochContraction_concat.sfs ${sample_size} ../Analysis/ThreeEpochContraction_${sample_size}/syn
-python downsample_sfs.py ../Simulations/simple_simulations/TwoEpochExpansion_concat.sfs ${sample_size} ../Analysis/TwoEpochExpansion_${sample_size}/syn
-python downsample_sfs.py ../Simulations/simple_simulations/ThreeEpochExpansion_concat.sfs ${sample_size} ../Analysis/ThreeEpochExpansion_${sample_size}/syn
-python downsample_sfs.py ../Simulations/simple_simulations/ThreeEpochBottleneck_concat.sfs ${sample_size} ../Analysis/ThreeEpochBottleneck_${sample_size}/syn
+# python downsample_sfs.py ../Simulations/simple_simulations/TwoEpochContraction_concat.sfs ${sample_size} ../Analysis/TwoEpochContraction_${sample_size}/syn
+# python downsample_sfs.py ../Simulations/simple_simulations/ThreeEpochContraction_concat.sfs ${sample_size} ../Analysis/ThreeEpochContraction_${sample_size}/syn
+# python downsample_sfs.py ../Simulations/simple_simulations/TwoEpochExpansion_concat.sfs ${sample_size} ../Analysis/TwoEpochExpansion_${sample_size}/syn
+# python downsample_sfs.py ../Simulations/simple_simulations/ThreeEpochExpansion_concat.sfs ${sample_size} ../Analysis/ThreeEpochExpansion_${sample_size}/syn
+# python downsample_sfs.py ../Simulations/simple_simulations/ThreeEpochBottleneck_concat.sfs ${sample_size} ../Analysis/ThreeEpochBottleneck_${sample_size}/syn
