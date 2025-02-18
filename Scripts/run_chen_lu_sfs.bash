3f6e3c0e891a985639bef61e1b1596c868ce4612
jon-mah <jonmah@uw.edu>
2025-02-18 11:05:43 -0800

    masked bi-allelic vcf files

--- Scripts/run_chen_lu_sfs.bash
+++ Scripts/run_chen_lu_sfs.bash
@@ -6,16 +6,15 @@
 #$ -o /u/home/j/jonmah/postproc_output
 #$ -l h_rt=12:00:00
 #$ -l highp
-#$ -t 21
+#$ -t 1-22
 
-SGE_TASK_ID='21'
+# SGE_TASK_ID='21'
+
 chr='chr'$SGE_TASK_ID
-
-# chr='chr21'
 dirvcf='/u/project/klohmuel/DataRepository/NYGC_1KG/genotype'
 inputvcf=$dirvcf"/20201028_CCDG_14151_B01_GRM_WGS_2020-08-05_"$chr".recalibrated_variants.vcf.gz"
 maskfile='/u/project/klohmuel/cdi/noncdoingdfe/data/vcf_1kg220425/originalfile/originalfile/20160622.allChr.mask.bed'
-inputsample='../Data/popfile_EUR_305.txt'
-dirout='../Data/1KG_EUR_updated/'
+inputsample='../Data/samples_EUR.txt'
+dirout='../Data/'
 
-bash ./uti_human_snps_SFS.bash -c chr21 -v $inputvcf -s $inputsample -m $maskfile -o $dirout
+bash ./uti_human_snps_SFS.bash -c $chr -v $inputvcf -s $inputsample -m $maskfile -o $dirout
