3c866447bb0bd275b4ef0a2714c126455194f814
jon-mah <jonmah@uw.edu>
2025-02-27 15:10:26 -0800

    updated 1KG_EUR demographic models

--- Scripts/fit_demographic_model.bash
+++ Scripts/fit_demographic_model.bash
@@ -6,14 +6,15 @@
 #$ -l highp
 #$ -e /u/home/j/jonmah/postproc_error
 #$ -o /u/home/j/jonmah/postproc_output
-#$ -N fit_gnomAD_700
+#$ -N fit_updated_1KG_300
 #$ -t 300
 
-# SGE_TASK_ID=50
+# SGE_TASK_ID=10
 
 sample_size=$SGE_TASK_ID
 
 # python fit_demographic_model.py ../Analysis/p_copri_core_${sample_size}/syn_downsampled_sfs.txt ../Analysis/p_copri_core_${sample_size}/
 # python fit_demographic_model.py ../Analysis/1kg_EUR_${sample_size}/syn_downsampled_sfs.txt ../Analysis/1kg_EUR_${sample_size}/
 # python fit_demographic_model.py ../Analysis/ooa_simulated_${sample_size}/syn_downsampled_sfs.txt ../Analysis/ooa_simulated_${sample_size}/
-python fit_demographic_model.py ../Analysis/gnomAD_${sample_size}/syn_downsampled_sfs.txt ../Analysis/gnomAD_${sample_size}/
+# python fit_demographic_model.py ../Analysis/gnomAD_${sample_size}/syn_downsampled_sfs.txt ../Analysis/gnomAD_${sample_size}/
+python fit_demographic_model.py ../Data/1KG_2020/${sample_size}_syn_downsampled_sfs.txt ../Analysis/updated_1KG_EUR_${sample_size}/
