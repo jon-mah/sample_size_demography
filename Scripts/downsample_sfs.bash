2d567b3296bfc68437f37894b1f1d316e24bffcf
jon-mah <jonmah@uw.edu>
2025-03-06 09:34:03 -0800

    Downsampled nonsynonymous SFSs

--- Scripts/downsample_sfs.bash
+++ Scripts/downsample_sfs.bash
@@ -21,11 +21,12 @@ sample_size=$SGE_TASK_ID
 # python downsample_sfs.py ../Data/kim_et_al_2017_1kg_nonsyn.txt ${sample_size} ../Analysis/1kg_EUR_${sample_size}/nonsyn
 
 # 1000Genomes EUR 2020 data
-python downsample_sfs.py ../Data/1KG_2020/syn_chr_concat.sfs ${sample_size} ../Data/1KG_2020/${sample_size}_syn
-python downsample_sfs.py ../Data/1KG_2020/nonsyn_chr_concat.sfs ${sample_size} ../Data/1KG_2020/${sample_size}_nonsyn
+# python downsample_sfs.py ../Data/1KG_2020/syn_chr_concat.sfs ${sample_size} ../Data/1KG_2020/${sample_size}_syn
+# python downsample_sfs.py ../Data/1KG_2020/nonsyn_chr_concat.sfs ${sample_size} ../Data/1KG_2020/${sample_size}_nonsyn
 
 # gnomAD EUR data
 # python downsample_sfs.py ../Data/gnomAD_empirical_syn_sfs.txt ${sample_size} ../Analysis/gnomAD_${sample_size}/syn
+python downsample_sfs.py ../Data/gnomAD_empirical_nonsyn_sfs.txt ${sample_size} ../Analysis/gnomAD_${sample_size}/nonsyn
 
 # Simulated Tennessen data
 # python downsample_sfs.py ../Simulations/ooa_864_sfs/dadi/pop1.sfs ${sample_size} ../Analysis/ooa_simulated_${sample_size}/syn
