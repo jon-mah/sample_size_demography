ab4eb828261cd93e500ae30c62ceb813026cda7f
jon-mah <jonmah@uw.edu>
2025-05-05 19:26:03 -0700

    small_nu contraction

--- Scripts/generate_pops_file.py
+++ Scripts/generate_pops_file.py
@@ -1,9 +1,9 @@
 def generate_output(filename):
     with open(filename, 'w') as f:
-        for i in range(7500):
+        for i in range(5000):
             f.write(f"tsk_{i}\tpop1\n")
 
 if __name__ == "__main__":
-    output_filename = "sample_7500_pops.txt"
+    output_filename = "sample_5000_pops.txt"
     generate_output(output_filename)
     print(f"Output generated to {output_filename}")
