b452a4adfaf4f93ef01278e5d677fc1d6436573d
jon-mah <jonmah@uw.edu>
2025-06-18 16:15:20 -0700

    sfs summary statistics

--- Scripts/compute_sfs_summary_statistics.bash
+++ Scripts/compute_sfs_summary_statistics.bash
@@ -6,15 +6,22 @@
 #$ -o /u/home/j/jonmah/postproc_output
 #$ -l h_data=1G
 #$ -l h_rt=00:10:00
-#$ -t 10-1000:10
+#$ -t 11-1001:10
 
 # SGE_TASK_ID=10
 
 sample_size=$SGE_TASK_ID
 
 # Simple simulations
-python compute_sfs_summary_statistics.py ../Simulations/simple_simulations/TwoEpochContraction_${sample_size}_concat.sfs ../Simulations/simple_simulations/TwoEpochContraction_${sample_size}_concat
-python compute_sfs_summary_statistics.py ../Simulations/simple_simulations/ThreeEpochContraction_${sample_size}_concat.sfs ../Simulations/simple_simulations/ThreeEpochContraction_${sample_size}_concat
-python compute_sfs_summary_statistics.py ../Simulations/simple_simulations/TwoEpochExpansion_${sample_size}_concat.sfs ../Simulations/simple_simulations/TwoEpochExpansion_${sample_size}_concat
-python compute_sfs_summary_statistics.py ../Simulations/simple_simulations/ThreeEpochExpansion_${sample_size}_concat.sfs ../Simulations/simple_simulations/ThreeEpochExpansion_${sample_size}_concat
-python compute_sfs_summary_statistics.py ../Simulations/simple_simulations/ThreeEpochBottleneck_${sample_size}_concat.sfs ../Simulations/simple_simulations/ThreeEpochBottleneck_${sample_size}_concat
+# python compute_sfs_summary_statistics.py ../Simulations/simple_simulations/TwoEpochContraction_${sample_size}_concat.sfs ../Simulations/simple_simulations/TwoEpochContraction_${sample_size}_concat
+# python compute_sfs_summary_statistics.py ../Simulations/simple_simulations/ThreeEpochContraction_${sample_size}_concat.sfs ../Simulations/simple_simulations/ThreeEpochContraction_${sample_size}_concat
+# python compute_sfs_summary_statistics.py ../Simulations/simple_simulations/TwoEpochExpansion_${sample_size}_concat.sfs ../Simulations/simple_simulations/TwoEpochExpansion_${sample_size}_concat
+# python compute_sfs_summary_statistics.py ../Simulations/simple_simulations/ThreeEpochExpansion_${sample_size}_concat.sfs ../Simulations/simple_simulations/ThreeEpochExpansion_${sample_size}_concat
+# python compute_sfs_summary_statistics.py ../Simulations/simple_simulations/ThreeEpochBottleneck_${sample_size}_concat.sfs ../Simulations/simple_simulations/ThreeEpochBottleneck_${sample_size}_concat
+
+# Lynch theoretical SFS
+python compute_sfs_summary_statistics.py ../Analysis/optimal_sample_size_test/2epC_${sample_size}_expected_sfs.txt ../Analysis/optimal_sample_size_test/2epC_${sample_size}
+python compute_sfs_summary_statistics.py ../Analysis/optimal_sample_size_test/2epE_${sample_size}_expected_sfs.txt ../Analysis/optimal_sample_size_test/2epE_${sample_size}
+python compute_sfs_summary_statistics.py ../Analysis/optimal_sample_size_test/3epC_${sample_size}_expected_sfs.txt ../Analysis/optimal_sample_size_test/3epC_${sample_size}
+python compute_sfs_summary_statistics.py ../Analysis/optimal_sample_size_test/3epE_${sample_size}_expected_sfs.txt ../Analysis/optimal_sample_size_test/3epE_${sample_size}
+python compute_sfs_summary_statistics.py ../Analysis/optimal_sample_size_test/3epB_${sample_size}_expected_sfs.txt ../Analysis/optimal_sample_size_test/3epB_${sample_size}
