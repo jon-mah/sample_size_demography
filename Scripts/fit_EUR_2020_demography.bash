1682222ec39c62aa96397be07553ef6e940099fa
jon-mah <jonmah@uw.edu>
2025-09-03 07:45:57 -0700

    one-epoch

--- Scripts/fit_EUR_2020_demography.bash
+++ Scripts/fit_EUR_2020_demography.bash
@@ -2,14 +2,13 @@
 #$ -cwd
 #$ -V
 #$ -l h_data=50G
-#$ -l h_rt=72:00:00
-#$ -l highp
+#$ -l h_rt=0:10:00
 #$ -e /u/home/j/jonmah/postproc_error
 #$ -o /u/home/j/jonmah/postproc_output
 #$ -N EUR_demog
 #$ -t 200-300:10
 
-SGE_TASK_ID=240
+# SGE_TASK_ID=240
 
 sample_size=$SGE_TASK_ID
 
@@ -21,4 +20,5 @@ sample_size=$SGE_TASK_ID
 
 # 1KG EUR 2020
 # python fit_demographic_model.py ../Analysis/1kg_EUR_2020_${sample_size}/syn_downsampled_sfs.txt ../Analysis/1kg_EUR_2020_${sample_size}/ --sum_alleles --model_type two_epoch
-python fit_demographic_model.py ../Analysis/1kg_EUR_2020_${sample_size}/syn_downsampled_sfs.txt ../Analysis/1kg_EUR_2020_${sample_size}/ --sum_alleles --model_type three_epoch --input_params 0.68744936 15.31011472 0.13714748  0.02801032
+# python fit_demographic_model.py ../Analysis/1kg_EUR_2020_${sample_size}/syn_downsampled_sfs.txt ../Analysis/1kg_EUR_2020_${sample_size}/ --sum_alleles --model_type three_epoch --input_params 0.68744936 15.31011472 0.13714748  0.02801032
+python fit_demographic_model.py ../Analysis/1kg_EUR_2020_${sample_size}/syn_downsampled_sfs.txt ../Analysis/1kg_EUR_2020_${sample_size}/ --sum_alleles --model_type one_epoch
