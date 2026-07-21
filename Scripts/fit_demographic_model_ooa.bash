a3c21696ea72c2c93db548ee3dc7092304712653
jon-mah <jonmah@uw.edu>
2026-07-21 12:06:00 -0700

    tennessen simulations + output

--- Scripts/fit_demographic_model_ooa.bash
+++ Scripts/fit_demographic_model_ooa.bash
@@ -2,18 +2,14 @@
 #$ -cwd
 #$ -V
 #$ -l h_data=50G
-#$ -l h_rt=72:00:00
-#$ -l highp
+#$ -l h_rt=4:00:00
 #$ -e /u/home/j/jonmah/postproc_error
 #$ -o /u/home/j/jonmah/postproc_output
 #$ -N fit_ooa
-#$ -t 700
+#$ -t 10-800:10
 
-# SGE_TASK_ID=50
+SGE_TASK_ID=770
 
 sample_size=$SGE_TASK_ID
 
-# python fit_demographic_model.py ../Analysis/p_copri_core_${sample_size}/syn_downsampled_sfs.txt ../Analysis/p_copri_core_${sample_size}/
-# python fit_demographic_model.py ../Analysis/1kg_EUR_${sample_size}/syn_downsampled_sfs.txt ../Analysis/1kg_EUR_${sample_size}/
-python fit_demographic_model.py ../Analysis/ooa_simulated_${sample_size}/syn_downsampled_sfs.txt ../Analysis/ooa_simulated_${sample_size}/
-# python fit_demographic_model.py ../Analysis/gnomAD_${sample_size}/syn_downsampled_sfs.txt ../Analysis/gnomAD_${sample_size}/
+python fit_demographic_model.py ../Simulations/tennessen/ooa_${sample_size}_sfs/dadi/pop1.sfs ../Analysis/tennessen_ooa_${sample_size}/ --model_type two_epoch
--- Scripts/tennessen_simulations.bash
+++ Scripts/tennessen_simulations.bash
@@ -1,17 +1,20 @@
 #!/bin/bash
-#$ -N downsample_sfs.bash
+#$ -N tennessen_sim.bash
 #$ -cwd # Run qsub script from desired working directory
 #$ -V
 #$ -e /u/home/j/jonmah/postproc_error
 #$ -o /u/home/j/jonmah/postproc_output
 #$ -l h_data=60G
-#$ -l h_rt=01:30:00
+#$ -l h_rt=02:00:00
+#$ -t 10-800:10
 
-stdpopsim HomSap -L 1000000 -o ../Simulations/ooa_864.trees -s 1 -d OutOfAfrica_2T12 EUR:1000
+# SGE_TASK_ID=10
 
-outfile='../Simulations/ooa_864'
+sample_size=$SGE_TASK_ID
+
+stdpopsim HomSap -L 1000000 -o ../Simulations/tennessen/ooa_${sample_size}.trees -s 1 -d OutOfAfrica_2T12 EUR:1000
 
 # Convert msprime .trees to .vcf format
-tskit vcf ../Simulations/${outfile}.trees > ../Simulations/${outfile}.vcf
+tskit vcf ../Simulations/tennessen/ooa_${sample_size}.trees > ../Simulations/tennessen/ooa_${sample_size}.vcf
 
-easySFS.py -a -f -i ../Simulations/${outfile}.vcf -p sample_1000_pops.txt -o ../Simulations/${outfile}_sfs --proj 864
+easySFS.py -a -f -i ../Simulations/tennessen/ooa_${sample_size}.vcf -p sample_${sample_size}_pops.txt -o ../Simulations/tennessen/ooa_${sample_size}_sfs --proj ${sample_size}
